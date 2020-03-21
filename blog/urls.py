c3d07b1468fbc41b7827d77fda805b4bf1dbec1d
Murilo <murilo.martinss@hotmail.com>
2020-03-21 17:37:13 -0300

    Ajustes nas urls, cria__o de mais vistos, modificação de nomes e alteração no modelo

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
